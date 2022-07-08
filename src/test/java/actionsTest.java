38dcb8abe60abf1b136efb6fa8d99126b0deb750
mariapanoti <97898942+mariapanoti@users.noreply.github.com>
2022-07-08 19:23:53 +0300

    Update actionsTest.java

--- src/test/java/actionsTest.java
+++ src/test/java/actionsTest.java
@@ -23,7 +23,7 @@ public class actionsTest
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-      //  options.addArguments("--headless");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.navigate().to("https://www.google.com");
         driver.manage().window().maximize();
